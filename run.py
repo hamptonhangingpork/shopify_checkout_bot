471c630c827a9a5c3c06608087d32c82daa5736c
hamptonhangingpork <82765262+hamptonhangingpork@users.noreply.github.com>
2021-04-19 14:55:28 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,7 +1,83 @@
 import requests, webbrowser, time, re, json, os
 from bs4 import BeautifulSoup as Soup
 from urllib.parse import urljoin
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
+def checkOut(url):
+	driver = webdriver.Chrome(configJson['MAIN']['CHROMEDRIVER'])
+	driver.get(url)
+	
+	Select(driver.find_element_by_id("checkout_shipping_address_country")).select_by_value(configJson['CHECKOUT']['SHIPPING']['COUNTRY'])
+	
+	#Ignore countries with no state field
+	try:
+		Select(driver.find_element_by_id("checkout_shipping_address_province")).select_by_visible_text(configJson['CHECKOUT']['SHIPPING']['STATE'])
+	except:
+		pass
+		
+	driver.find_element_by_id("checkout_email_or_phone").send_keys(configJson['CHECKOUT']['SHIPPING']['EMAIL'])
+	driver.find_element_by_id("checkout_shipping_address_first_name").send_keys(configJson['CHECKOUT']['SHIPPING']['FIRSTNAME'])
+	driver.find_element_by_id("checkout_shipping_address_last_name").send_keys(configJson['CHECKOUT']['SHIPPING']['LASTNAME'])
+	driver.find_element_by_id("checkout_shipping_address_address1").send_keys(configJson['CHECKOUT']['SHIPPING']['ADDRESS1'])
+	if configJson['CHECKOUT']['SHIPPING']['ADDRESS2']:
+		driver.find_element_by_id("checkout_shipping_address_address2").send_keys(configJson['CHECKOUT']['SHIPPING']['ADDRESS2'])
+	driver.find_element_by_id("checkout_shipping_address_city").send_keys(configJson['CHECKOUT']['SHIPPING']['CITY'])
+	driver.find_element_by_id("checkout_shipping_address_zip").send_keys(configJson['CHECKOUT']['SHIPPING']['ZIP'])
+	driver.find_element_by_id("checkout_shipping_address_phone").send_keys(configJson['CHECKOUT']['SHIPPING']['PHONE'])
+	
+	#Proceed to shipping options
+	driver.find_element_by_id('continue_button').click()
+	
+	#Proceed to billing
+	driver.find_element_by_id('continue_button').click()
+	
+	driver.switch_to.frame(driver.find_element_by_xpath("//*[contains(@id, 'card-fields-number-')]"))
+	for number in configJson['CARD']['NUMBER']:
+		driver.find_element_by_id("number").send_keys(number);
+	driver.switch_to.parent_frame();
+
+	driver.switch_to.frame(driver.find_element_by_xpath("//*[contains(@id, 'card-fields-name-')]"))
+	driver.find_element_by_id("name").send_keys(configJson['CARD']['NAME']);
+	driver.switch_to.parent_frame();
+
+	driver.switch_to.frame(driver.find_element_by_xpath("//*[contains(@id, 'card-fields-expiry-')]"))
+	for number in configJson['CARD']['EXP']:
+		driver.find_element_by_id("expiry").send_keys(number);
+	driver.switch_to.parent_frame();
+
+	driver.switch_to.frame(driver.find_element_by_xpath("//*[contains(@id, 'card-fields-verification_value-')]"))
+	driver.find_element_by_id("verification_value").send_keys(configJson['CARD']['SEC']);
+	driver.switch_to.parent_frame();
+	
+	driver.find_element_by_id("checkout_different_billing_address_true").click()
+	Select(driver.find_element_by_id("checkout_billing_address_country")).select_by_visible_text(configJson['CHECKOUT']['BILLING']['COUNTRY'])
+	
+	#Ignore countries with no state field
+	try:
+		Select(driver.find_element_by_id("checkout_billing_address_province")).select_by_visible_text(configJson['CHECKOUT']['BILLING']['STATE'])
+	except:
+		pass
+		
+	driver.find_element_by_id("checkout_billing_address_first_name").send_keys(configJson['CHECKOUT']['BILLING']['FIRSTNAME'])
+	driver.find_element_by_id("checkout_billing_address_last_name").send_keys(configJson['CHECKOUT']['BILLING']['LASTNAME'])
+	driver.find_element_by_id("checkout_billing_address_address1").send_keys(configJson['CHECKOUT']['BILLING']['ADDRESS1'])
+	if configJson['CHECKOUT']['BILLING']['ADDRESS2']:
+		driver.find_element_by_id("checkout_billing_address_address2").send_keys(configJson['CHECKOUT']['BILLING']['ADDRESS2'])
+	driver.find_element_by_id("checkout_billing_address_city").send_keys(configJson['CHECKOUT']['BILLING']['CITY'])
+	driver.find_element_by_id("checkout_billing_address_zip").send_keys(configJson['CHECKOUT']['BILLING']['ZIP'])
+	driver.find_element_by_id("checkout_billing_address_phone").send_keys(configJson['CHECKOUT']['BILLING']['PHONE'])
+	
+	#Proceed to order review page
+	driver.find_element_by_id('continue_button').click()
+	
+	#Pay now (Uncomment the next line if you want the script to auto checkout)
+	#driver.find_element_by_id('continue_button').click()
+	
+	#Selenium closes the browser once script execution is finished so I added 5 min sleep timer so you can check order details 
+	time.sleep(300)
+	
 def checkSite():
 	loopFlag = True		
 	while loopFlag:
@@ -31,14 +107,26 @@ def checkSite():
 									if variantFlag:
 										name = tag.find("id").text
 										print(f"Check·out {variantFlag[0]} with product id {name}")
-										webbrowser.get('chrome').open(urljoin(configJson['MAIN']['BASE_LINK'], configJson['MAIN']['CART_LINK']) + f"/{name}:{configJson['MAIN']['QUANTITY']}")
+										if not configJson['MAIN']['CHECKOUT']:
+											webbrowser.register('chrome',
+											None,
+											webbrowser.BackgroundBrowser(configJson['MAIN']['CHROME']))
+											webbrowser.get('chrome').open(urljoin(configJson['MAIN']['BASE_LINK'], configJson['MAIN']['CART_LINK']) + f"/{name}:{configJson['MAIN']['QUANTITY']}")
+										else:
+											checkOut(urljoin(configJson['MAIN']['BASE_LINK'], configJson['MAIN']['CART_LINK']) + f"/{name}:{configJson['MAIN']['QUANTITY']}")
 										loopFlag = False
 										linkOpened = True
 										break
 								else:
 									name = tag.find("id").text
 									print(f"Check·out {productTitle} with product id {name}")
-									webbrowser.get('chrome').open(urljoin(configJson['MAIN']['BASE_LINK'], configJson['MAIN']['CART_LINK']) + f"/{name}:{configJson['MAIN']['QUANTITY']}")
+									if not configJson['MAIN']['CHECKOUT']:
+										webbrowser.register('chrome',
+											None,
+											webbrowser.BackgroundBrowser(configJson['MAIN']['CHROME']))
+										webbrowser.get('chrome').open(urljoin(configJson['MAIN']['BASE_LINK'], configJson['MAIN']['CART_LINK']) + f"/{name}:{configJson['MAIN']['QUANTITY']}")
+									else:
+										checkOut(urljoin(configJson['MAIN']['BASE_LINK'], configJson['MAIN']['CART_LINK']) + f"/{name}:{configJson['MAIN']['QUANTITY']}")
 									loopFlag = False
 									linkOpened = True
 									break
@@ -52,9 +140,4 @@ def checkSite():
 if __name__ == "__main__":
 	with open(os.path.join(os.getcwd(), "config.json")) as jsonHandle:
 		configJson = json.load(jsonHandle)
-
-	webbrowser.register('chrome',
-		None,
-		webbrowser.BackgroundBrowser(configJson['MAIN']['CHROME']))
-	
 	checkSite()
